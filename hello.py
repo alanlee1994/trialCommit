0397167d41b4777f0d56b131a0a414a34c370897
alanlee1994 <alan0014@e.ntu.edu.sg>
2020-07-12 17:55:28 +0800

    second push

--- hello.py
+++ hello.py
@@ -3,6 +3,11 @@ import pandas as pd
 def say_hello():
     print("hello")
 
+def do_math(x,y):
+    return x+y
 
 if __name__ == "__main__":
     say_hello()
+    x=1
+    y=3
+    print(do_math(x,y))
